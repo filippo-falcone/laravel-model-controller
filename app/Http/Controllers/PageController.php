bc86916b089e4ff50d50c8a18268c26d8323c543
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-21 17:47:06 +0200

    feat: add index function to PageController and add route to web

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::all();
+
+       dd($movies);
+        return view('home');
+    }
 }
